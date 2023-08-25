41f88b86891e46b87094d918faba4de10914e10c
Carrara1 <joao.queirozcarrara@gmail.com>
2023-08-25 20:16:12 -0300

    Updated

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -1,12 +1,6 @@
 const AboutPage = () => {
 	return (
 		<div className="about-page">
-			<div className="profile-picture">
-				<img
-					src="https://static-wp-tor15-prd.torcedores.com/wp-content/uploads/2022/08/rony-palmeiras.png"
-					alt="Profile"
-				/>
-			</div>
 			<div className="bio">
 				<h2>Lorem Ipsum</h2>
 				<p>Test</p>
