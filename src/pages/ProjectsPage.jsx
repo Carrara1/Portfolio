fe83460b4c73c5762f2f2403b1aa620c4ef49c56
Carrara1 <joao.queirozcarrara@gmail.com>
2024-08-02 19:50:47 -0300

    Added a new project container.

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -9,27 +9,29 @@ function ProjectsPage() {
 					<p className="p-projects">Explore meu portfólio.</p>
 					<br />
 					<div className="projects-wrapper">
-						<ProjectContainer
+						{/* <ProjectContainer
 							imageSrc="https://github.com/Carrara1/Portfolio/blob/main/src/assets/triplesclone.png?raw=true"
 							title="triplescosmos-clone"
 							text="Um clone do website oficial do grupo de K-Pop tripleS, com todas as funcionalidades."
 							btnUrl="https://example.com"
 							btnText="Acessar"
-						/>
+						/> */}
 						<ProjectContainer
 							imageSrc="https://github.com/Carrara1/Portfolio/blob/main/src/assets/projectshp.png?raw=true"
 							title="Portfólio"
-							text="Link para o GitHub do meu portfólio. Foi refeito em ReactJS, após ter realizado a primeira versão em JavaScript vanilla. "
+							text="Link para o GitHub do meu portfólio. Foi refeito em ReactJS, após ter realizado a primeira versão em JavaScript vanilla. 
+							Tecnologias usadas: ReactJS, Bootstrap"
 							btnUrl="https://github.com/Carrara1/Portfolio"
 							btnText="Acessar"
 						/>
-						{/* <ProjectContainer
+						<ProjectContainer
 							imageSrc="https://github.com/Carrara1/Portfolio/blob/main/src/assets/triplesclone.png?raw=true"
-							title="triplescosmos-clone"
-							text="Um clone do website oficial do grupo de K-Pop tripleS, com todas as funcionalidades."
-							btnUrl="https://example.com" 
+							title="Sistema de chatrooms em ReactJS e NodeJS"
+							text="Um sistema full-stack de chatroom com opções de criar e entrar em diversos chats, com o sistema de chat instantâneo.
+							Tecnologias usadas: ReactJS, NodeJS	"
+							btnUrl="https://example.com"
 							btnText="Acessar"
-						/> */}
+						/>
 					</div>
 				</div>
 			</div>
